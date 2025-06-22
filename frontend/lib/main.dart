64f02daa8907f208a8d67da041f6f938e97f0dc5
Nanda Prayoga <93271658+Naryou99@users.noreply.github.com>
2025-06-22 11:29:01 +0700

    Update main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/image_upscaler_screen.dart';
-import 'package:google_fonts/google_fonts.dart'; // <-- IMPORT BARU
+import 'package:google_fonts/google_fonts.dart';
 
 
 void main() {
@@ -26,29 +26,22 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    // Mengambil tema dasar
     final textTheme = Theme.of(context).textTheme;
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'RealScale SR',
-
-      // Menerapkan Poppins ke tema terang
       theme: ThemeData(
         brightness: Brightness.light,
         primarySwatch: Colors.deepPurple,
         scaffoldBackgroundColor: Colors.grey[100],
         cardColor: Colors.white,
-        // Menggunakan GoogleFonts untuk seluruh tema teks
         textTheme: GoogleFonts.poppinsTextTheme(textTheme).apply(bodyColor: Colors.black87),
         primaryTextTheme: GoogleFonts.poppinsTextTheme(textTheme).apply(bodyColor: Colors.black87),
       ),
-
-      // Menerapkan Poppins ke tema gelap
       darkTheme: ThemeData(
         brightness: Brightness.dark,
         primarySwatch: Colors.deepPurple,
-        // Menggunakan GoogleFonts untuk seluruh tema teks
         textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).primaryTextTheme),
       ),
 
